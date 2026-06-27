2bfddf6790a7e7047ab2ddfe48357ffeb926b800
Khaira001 <kadirikhaira1206@gmail.com>
2026-06-27 16:56:29 +0100

    Add files via upload

---
+++ calculatorpython/calculator.py
@@ -0,0 +1,33 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error"
+    return a / b
+
+def modulus(a, b):
+    return a % b
+
+def power(a):
+    return a ** 2
+
+def square_root(a):
+    return math.sqrt(a)
+
+def sine(a):
+    return math.sin(math.radians(a))
+
+def cosine(a):
+    return math.cos(math.radians(a))
+
+def tangent(a):
+    return math.tan(math.radians(a))
